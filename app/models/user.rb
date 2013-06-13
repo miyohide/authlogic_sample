a4bd30bafacbb9d799f5a4fc2943304e56c0272e
Hidenori Miyoshi <miyohide@gmail.com>
2013-06-13 21:43:44 +0900

    テーブル定義でnot null設定していたカラムを入力できるように設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :name, :email, :login, :password, :password_confirmation
   acts_as_authentic do |c|
     c.login_field = 'email'
   end
