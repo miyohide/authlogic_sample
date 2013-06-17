a83fae14e0624b0891d1e6088dc8c7fa8ac7b35b
Hidenori Miyoshi <miyohide@gmail.com>
2013-06-17 22:49:14 +0900

    add role_id to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130613110238) do
+ActiveRecord::Schema.define(:version => 20130617134745) do
 
   create_table "entries", :force => true do |t|
     t.string   "title"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(:version => 20130613110238) do
     t.string   "last_login_ip"
     t.datetime "created_at",                          :null => false
     t.datetime "updated_at",                          :null => false
+    t.integer  "role_id"
   end
 
 end
